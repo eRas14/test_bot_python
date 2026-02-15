0293c4991e0ba39ac98bfa7b083247fdb28cf8bd
Aibek Urazov PC <ibek-94@mail.ru>
2026-02-15 22:29:41 +0500

    add keyboard.py

---
+++ keyboards.py
@@ -0,0 +1,66 @@
+from aiogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
+                           KeyboardButton, ReplyKeyboardMarkup)
+
+
+# Создание обычной клавиатуры (ReplyKeyboard)
+def get_reply_keyboard():
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(
+                    text="Я красная кнопка",
+                    style="danger",
+                    icon_custom_emoji_id="5875323095501247167",
+                )
+            ],
+            [
+                KeyboardButton(
+                    text="Я синия кнопка",
+                    style="primary",
+                    icon_custom_emoji_id="5197247368344379154",
+                )
+            ],
+            [
+                KeyboardButton(
+                    text="Я зеленая кнопки ",
+                    style="success",
+                    icon_custom_emoji_id="5388977792224342481",
+                )
+            ],
+        ],
+        resize_keyboard=True,  # Автоматически подгонять размер
+        input_field_placeholder="Выберите действие...",
+    )
+    return keyboard
+
+
+def get_inline_keyboard():
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Мой Youtube кана",
+                    url="https://t.me/erasj",
+                    style="danger",
+                    icon_custom_emoji_id="5411222239599168125",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="Это тоже я",
+                    url="https://t.me/erasj",
+                    style="primary",
+                    icon_custom_emoji_id="5884356309372900807",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="Вацап",
+                    url="https://t.me/erasj",
+                    style="success",
+                    icon_custom_emoji_id="5388888315170665675",
+                )
+            ],
+        ]
+    )
+    return keyboard
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import asyncio
 import logging
 import os
-from tkinter.ttk import Style
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
-                           KeyboardButton, Message, ReplyKeyboardMarkup)
+from aiogram.types import Message
 from dotenv import load_dotenv
+
+from keyboards import get_inline_keyboard, get_reply_keyboard
 
 load_dotenv()
 
@@ -20,70 +20,6 @@ BOT_TOKEN = os.getenv("TOKEN_BOT")
 # Инициализация бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-
-
-# Создание обычной клавиатуры (ReplyKeyboard)
-def get_reply_keyboard():
-    keyboard = ReplyKeyboardMarkup(
-        keyboard=[
-            [
-                KeyboardButton(
-                    text="Я красная кнопка",
-                    style="danger",
-                    icon_custom_emoji_id="5875323095501247167",
-                )
-            ],
-            [
-                KeyboardButton(
-                    text="Я синия кнопка",
-                    style="primary",
-                    icon_custom_emoji_id="5197247368344379154",
-                )
-            ],
-            [
-                KeyboardButton(
-                    text="Я зеленая кнопки ",
-                    style="success",
-                    icon_custom_emoji_id="5388977792224342481",
-                )
-            ],
-        ],
-        resize_keyboard=True,  # Автоматически подгонять размер
-        input_field_placeholder="Выберите действие...",
-    )
-    return keyboard
-
-
-def get_inline_keyboard():
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="Мой Youtube кана",
-                    url="https://t.me/erasj",
-                    style="danger",
-                    icon_custom_emoji_id="5411222239599168125",
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="Это тоже я",
-                    url="https://t.me/erasj",
-                    style="primary",
-                    icon_custom_emoji_id="5884356309372900807",
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="Вацап",
-                    url="https://t.me/erasj",
-                    style="success",
-                    icon_custom_emoji_id="5388888315170665675",
-                )
-            ],
-        ]
-    )
-    return keyboard
 
 
 # Обработчик команды /start
